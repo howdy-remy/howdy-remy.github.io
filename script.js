aa44d6587d1529369414a46715f87f658f277e7a
Kimber Lynch <kmlynch714@gmail.com>
2022-06-26 18:28:22 -0400

    tracer content updates

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(function() {
     $('#front-end').click( () =>
         $('#intro__deets').replaceWith(
             `<p id='intro__deets'>
-                I’m the Software Engineering Manager for the front-end team at Tracer, a marketing data aggregation and analytics platform. I get jazzed by design systems and pattern libraries, and delivering tools and processes focused on the transition from idea to design to code. Currently, I’m working on converting an Ember/Coffeescript application to React, Redux, and Storybook.
+                        I recently wrapped up as an Senior Engineer at Ro, a telehealth company, where I worked on projects from member retention to being the only frontend engineer on the launch of their Mental Health offering, Ro Mind. Before that I was the Software Engineering Manager for the front-end team at Tracer, a marketing data aggregation and analytics platform, where I led their Ember-to-React conversion. I get jazzed by design systems and pattern libraries, and delivering tools and processes focused on the transition from idea to design to code.
             </p>`     
         ));
     $('#design').click( () =>
@@ -15,9 +15,11 @@ $(function() {
     $('#awesome').click( () =>
         $('#intro__deets').replaceWith(
             `<p id='intro__deets'>
-            Last year I backpacked over 150 miles including the Grand Canyon Rim to Rim to Rim&mdash;around 50 miles, 21,000 feet in elevation change, over 6 days.
+            In 2019 I backpacked over 150 miles including the Grand Canyon Rim to Rim to Rim&mdash;around 50 miles, 21,000 feet in elevation change, over 6 days.
             <br/><br/>
             In 2015 my husband and I converted a 1999 ford econoline e350 into a campervan. We took off in August and spent three months living and working in the van as we drove 13,000 miles around the country.
+            <br/><br/>
+            Nowadays I garden in the summer, build furniture in the winter, and play video games through the year.
             </p>`    
         ));
     $('.project').hover( 
